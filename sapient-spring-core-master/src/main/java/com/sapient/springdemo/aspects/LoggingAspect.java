560c6feff5ce49b1c9373788aff81345fd5d81c2
maitreyee290897 <maitreyee290897@gmail.com>
2019-07-15 16:55:01 +0530

    Spring AOP

---
+++ sapient-spring-core-master/src/main/java/com/sapient/springdemo/aspects/LoggingAspect.java
@@ -0,0 +1,18 @@
+package src.main.java.com.sapient.springdemo.aspects;
+
+import com.sapient.springdemo.service.EmployeeService;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Before("execution(* com.sapient.springdemo.service.EmployeeServiceImpl.saveEmployee(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("------------logging before the method is getting executed-------------------");
+        System.out.println(joinPoint.getTarget() instanceof EmployeeService);
+    }
+}
